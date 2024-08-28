a2e66ba75d0f1cee91794ec5dd4e0e240cdb5512
pppppark <juhyoung19990205@gmail.com>
2024-08-29 07:06:59 +0900

    UPDATE JavaScript File Name

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-// JavaScript
-window.onscroll = function() {headLock()};
+function validateEmailCheck(){
+  var email = document.getElementById("modal");
+  var valid_email = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-Za-z0-9\-]+/; 
+  //(알파벳,숫자)@(알파벳,숫자).(알파벳,숫자)
 
-var header = document.getElementById("head-line");
-var sticky = header.offsetTop;
-
-function headLock() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("fixed");
-  } else {
-    header.classList.remove("fixed");
+  if(valid_email.test(email.value)==false){ 
+    return false;
+  }
+  else {
+    alert("이메일이 정상적으로 입력되었습니다.");
   }
 }
